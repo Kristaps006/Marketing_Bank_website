e06fe2a43ca3fe4198fcfe31d9e37e9548b6016c
Your Name <kristaps006@gmail.com>
2021-03-03 13:12:26 +0100

    website done

--- script.js
+++ script.js
@@ -13,6 +13,7 @@ const tabs = document.querySelectorAll('.operations__tab');
 const tabsContainer = document.querySelector('.operations__tab-container');
 const tabsContent = document.querySelectorAll('.operations__content');
 const nav = document.querySelector('.nav');
+const dotContainer = document.querySelector('.dots');
 
 ///////////////////////////////////////////////////////
 
@@ -181,7 +182,7 @@ const sectionObserver = new IntersectionObserver(revealSection, {
 allSection.forEach(section => {
   // Here we observer all the sections
   sectionObserver.observe(section);
-  section.classList.add('section--hidden');
+  // section.classList.add('section--hidden');
 });
 
 /// LAZY LOADING IMAGES
@@ -196,7 +197,7 @@ const loadImg = (entries, observer) => {
 
   entry.target.src = entry.target.dataset.src;
 
-  // we liste for load event
+  // we liste for load event -load is like any other event
   entry.target.addEventListener('load', e => {
     entry.target.classList.remove('lazy-img');
   });
@@ -211,6 +212,96 @@ const imgObserver = new IntersectionObserver(loadImg, {
 });
 
 imgTargets.forEach(img => imgObserver.observe(img));
+
+////////////// SLIDER////////////////////////////////
+
+/* const slider = document.querySelector('.slider');
+slider.style.transform = 'scale(0.4) translateX(-600px)';
+slider.style.overflow = 'visible';
+ */
+const slider = function () {
+  const slides = document.querySelectorAll('.slide');
+  const btnLeft = document.querySelector('.slider__btn--left');
+  const btnRight = document.querySelector('.slider__btn--right');
+  const dotContainer = document.querySelector('.dots');
+
+  let curSlide = 0;
+  const maxSlide = slides.length;
+
+  // Functions
+  const createDots = function () {
+    slides.forEach(function (_, i) {
+      dotContainer.insertAdjacentHTML(
+        'beforeend',
+        `<button class="dots__dot" data-slide="${i}"></button>`
+      );
+    });
+  };
+
+  const activateDot = function (slide) {
+    document
+      .querySelectorAll('.dots__dot')
+      .forEach(dot => dot.classList.remove('dots__dot--active'));
+
+    document
+      .querySelector(`.dots__dot[data-slide="${slide}"]`)
+      .classList.add('dots__dot--active');
+  };
+
+  const goToSlide = function (slide) {
+    slides.forEach(
+      (s, i) => (s.style.transform = `translateX(${100 * (i - slide)}%)`)
+    );
+  };
+
+  // Next slide
+  const nextSlide = function () {
+    if (curSlide === maxSlide - 1) {
+      curSlide = 0;
+    } else {
+      curSlide++;
+    }
+
+    goToSlide(curSlide);
+    activateDot(curSlide);
+  };
+
+  const prevSlide = function () {
+    if (curSlide === 0) {
+      curSlide = maxSlide - 1;
+    } else {
+      curSlide--;
+    }
+    goToSlide(curSlide);
+    activateDot(curSlide);
+  };
+
+  const init = function () {
+    goToSlide(0);
+    createDots();
+
+    activateDot(0);
+  };
+  init();
+
+  // Event handlers
+  btnRight.addEventListener('click', nextSlide);
+  btnLeft.addEventListener('click', prevSlide);
+
+  document.addEventListener('keydown', function (e) {
+    if (e.key === 'ArrowLeft') prevSlide();
+    e.key === 'ArrowRight' && nextSlide();
+  });
+
+  dotContainer.addEventListener('click', function (e) {
+    if (e.target.classList.contains('dots__dot')) {
+      const { slide } = e.target.dataset;
+      goToSlide(slide);
+      activateDot(slide);
+    }
+  });
+};
+slider();
 
 /////////////////////////////////////////////////////////////////////////////////////////////////////////
 
@@ -430,3 +521,24 @@ console.log(h1.parentElement.children);
   if (el !== h1) el.style.transform = 'scale(0.5)';
 });
  */
+
+//___________EVENTS in Webpage life CYCLE //___________________________
+
+document.addEventListener('DOMContentLoaded', e => {
+  console.log('HTML parsed and DOm tree built', e);
+});
+
+window.addEventListener('load', e => {
+  console.log('page fully loaded', e);
+});
+
+// before we leave the page
+
+/* window.addEventListener('beforeunload', e => {
+  e.preventDefault();
+  console.log(e);
+  e.returnValue = 'message';
+});
+ */
+
+///////////// DIFFERENT WAYS OF LOADING JAVASCRIPT ///////////////
